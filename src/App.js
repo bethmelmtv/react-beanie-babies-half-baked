44292191346aa885938eeefa4b9f8a9cfbfc198b
Beth <bethmelmtv@gmail.com>
2022-05-09 15:32:12 -0700

    worked on different files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
 export default function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Switch>
-          <Route>
+          <Route exact path="/">
+            <BeaniesPage />
             {/* this home page route should list all the beanies */}
           </Route>
-          <Route> 
+
+          <Route exact path="/beanies/:id">
+            <BeanieDetail />
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
           </Route>
         </Switch>
--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom';
 
-export default function BeanieBaby({ beanieBaby }) {
+export default function BeanieBaby({ id, title, image }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <Link to={`/beanies/${id}`}>
+      <div>
+        <p>{title}</p>
+        <img src={image} />
+      </div>
+    </Link>
   );
 }
