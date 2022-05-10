e1eea1626d4deaa7a3149684887d5c9c49265d22
Beth <bethmelmtv@gmail.com>
2022-05-09 17:09:49 -0700

    worked on page

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,11 +4,13 @@ export default function BeanieBaby({ id, title, image }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    <Link to={`/beanies/${id}`}>
-      <div>
-        <p>{title}</p>
-        <img src={image} />
-      </div>
-    </Link>
+    <div className="beanie-item">
+      <Link to={`/beanies/${id}`}>
+        <div>
+          <p>{title}</p>
+          <img src={image} />
+        </div>
+      </Link>
+    </div>
   );
 }
